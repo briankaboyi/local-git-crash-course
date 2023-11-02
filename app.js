4da0c2867b4260b438ba977e0e3041554feb81c7
briankaboyi <briankaboyi0@gmail.com>
2023-11-02 10:22:41 +0300

    styling
    created a style.css document and added some styling, also modified the js to fit the styling

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const button = document.querySelector('.button');
 
 button.addEventListener('click', function (e) {
   const button = e.target;
-  if (button.style.background === 'blue') {
-    button.style.background = 'red';
+  if (button.style.background === 'rgb(121, 203, 214)') {
+    button.style.background = 'rgb(214, 121, 121)';
     button.style.color = 'white';
+    button.style.boxShadow = ' 0px 3px 10px -4px rgb(76, 87, 88)';
   } else {
-    button.style.background = 'blue';
+    button.style.background = 'rgb(121, 203, 214)';
     button.style.color = 'white';
+    button.style.boxShadow = ' 0px 3px 10px -4px rgb(76, 87, 88)';
   }
 });
